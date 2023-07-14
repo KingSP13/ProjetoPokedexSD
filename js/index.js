2ef13d63d96f94282ab93e310bcef30b286bd547
Igor Oliveira lima <ol.igor98@gmail.com>
2023-07-13 21:28:32 -0300

    navbar integration test3

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
   const triggerMenuBurger = document.getElementById('menuBurguer')
-const showBurguerItems = document.getElementById('burguerItems')
+  const showBurguerItems = document.getElementById('burguerItems')
 
-triggerMenuBurger.addEventListener('click', function() {
+  triggerMenuBurger.addEventListener('click', function() {
   triggerMenuBurger.classList.toggle('is-active')
   showBurguerItems.classList.toggle('is-active')
 });
