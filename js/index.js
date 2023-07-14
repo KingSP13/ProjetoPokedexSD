22fd81947643fe6676e1a6af8a0ac00e4da32d34
Igor Oliveira lima <ol.igor98@gmail.com>
2023-07-14 01:30:04 -0300

    type buttons included

--- js/index.js
+++ js/index.js
@@ -1,10 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const triggerMenuBurger = document.getElementById('menuBurguer')
-  const showBurguerItems = document.getElementById('burguerItems')
+const triggerMenuBurger = document.getElementById('menuBurger')
+const showBurguerItems = document.getElementById('burgerItems')
 
-  triggerMenuBurger.addEventListener('click', function() {
-  triggerMenuBurger.classList.toggle('is-active')
-  showBurguerItems.classList.toggle('is-active')
+triggerMenuBurger.addEventListener('click', function() {
+triggerMenuBurger.classList.toggle('is-active')
+showBurguerItems.classList.toggle('is-active')
 });
 
+
+
+
+
+const switchFire = document.getElementsByClassName('fireTheme')[0];
+const switchGrass = document.getElementsByClassName('grassTheme')[0];
+const switchWater = document.getElementsByClassName('waterTheme')[0];
+
+switchFire.addEventListener('click', function() {
+  switchFire.classList.toggle('fire')
 });
+switchGrass.addEventListener('click', function() {
+  switchGrass.classList.toggle('grass')
+});
+switchWater.addEventListener('click', function() {
+  switchWater.classList.toggle('water')
+});
+
+
+// fetch('../html/navbar.html')
+// .then(response => response.text())
+// .then(data => {
+// document.getElementById('navbar-placeholder').innerHTML = data;
+// });
--- js/navbarFetch.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('../html/navbar.html')
-    .then(response => response.text())
-    .then(data => {
-    document.getElementById('navbar-placeholder').innerHTML = data;
-});
-  });
-  
