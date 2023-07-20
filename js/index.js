1d9216454bb9753412469f893fcc8239741c0bf9
Igor Oliveira lima <ol.igor98@gmail.com>
2023-07-20 02:08:16 -0300

    3 types added and test for local storage

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// MENU BURGUER FUNCTIONS
+
 const triggerMenuBurger = document.getElementById('menuBurger')
 const showBurguerItems = document.getElementById('burgerItems')
 
@@ -18,28 +20,71 @@ buttonClose.onclick = function() {
   modal.close()
 }
 
+
+// THEME SWITCH FUNCTIONS
+
 const switchFire = document.getElementsByClassName('fireTheme')[0];
 const switchGrass = document.getElementsByClassName('grassTheme')[0];
 const switchWater = document.getElementsByClassName('waterTheme')[0];
+const switchDark = document.getElementsByClassName('darkTheme')[0];
+const switchElectric = document.getElementsByClassName('electricTheme')[0];
+const switchSteel = document.getElementsByClassName('steelTheme')[0];
 const theme = document.getElementsByTagName('body')[0];
 
 switchFire.addEventListener('click', function() {
   theme.classList.remove('palette-fire')
   theme.classList.remove('palette-grass')
   theme.classList.remove('palette-water')
+  theme.classList.remove('palette-dark')
+  theme.classList.remove('palette-elextric')
+  theme.classList.remove('palette-steel')
   theme.classList.add('palette-fire')
 });
 switchGrass.addEventListener('click', function() {
   theme.classList.remove('palette-fire')
   theme.classList.remove('palette-grass')
   theme.classList.remove('palette-water')
+  theme.classList.remove('palette-dark')
+  theme.classList.remove('palette-elextric')
+  theme.classList.remove('palette-steel')
   theme.classList.add('palette-grass')
 });
 switchWater.addEventListener('click', function() {
   theme.classList.remove('palette-fire')
   theme.classList.remove('palette-grass')
   theme.classList.remove('palette-water')
+  theme.classList.remove('palette-dark')
+  theme.classList.remove('palette-elextric')
+  theme.classList.remove('palette-steel')
   theme.classList.add('palette-water')
 });
+switchDark.addEventListener('click', function() {
+  theme.classList.remove('palette-fire')
+  theme.classList.remove('palette-grass')
+  theme.classList.remove('palette-water')
+  theme.classList.remove('palette-dark')
+  theme.classList.remove('palette-elextric')
+  theme.classList.remove('palette-steel')
+  theme.classList.add('palette-dark')
+});
+switchElectric.addEventListener('click', function() {
+  theme.classList.remove('palette-fire')
+  theme.classList.remove('palette-grass')
+  theme.classList.remove('palette-water')
+  theme.classList.remove('palette-dark')
+  theme.classList.remove('palette-elextric')
+  theme.classList.remove('palette-steel')
+  theme.classList.add('palette-electric')
+});
+switchSteel.addEventListener('click', function() {
+  theme.classList.remove('palette-fire')
+  theme.classList.remove('palette-grass')
+  theme.classList.remove('palette-water')
+  theme.classList.remove('palette-dark')
+  theme.classList.remove('palette-elextric')
+  theme.classList.remove('palette-steel')
+  theme.classList.add('palette-steel')
+});
+
 
 
--- teste - igor/js_teste.js
+++ teste - igor/js_teste.js
@@ -1,23 +1,52 @@
 const switchFire = document.getElementsByClassName('fireTheme')[0];
 const switchGrass = document.getElementsByClassName('grassTheme')[0];
 const switchWater = document.getElementsByClassName('waterTheme')[0];
-const theme = document.getElementsByTagName('body')[0];
+const themeDiv = document.getElementById('inputDiv');
 
 switchFire.addEventListener('click', function() {
-  theme.classList.remove('fire')
-  theme.classList.remove('grass')
-  theme.classList.remove('water')
-  theme.classList.add('fire')
+  themeDiv.dataset.tema = 'tema1';
 });
 switchGrass.addEventListener('click', function() {
-  theme.classList.remove('fire')
-  theme.classList.remove('grass')
-  theme.classList.remove('water')
-  theme.classList.add('grass')
+  themeDiv.dataset.tema = 'tema2';
 });
 switchWater.addEventListener('click', function() {
-  theme.classList.remove('fire')
-  theme.classList.remove('grass')
-  theme.classList.remove('water')
-  theme.classList.add('water')
+  themeDiv.dataset.tema = 'tema3';
 });
+
+// Função para salvar o tema padrão no localStorage
+function salvarTemaPadrao() {
+  const radios = document.getElementsByName("tema");
+  let temaSelecionado = null;
+
+  for (const radio of radios) {
+    if (radio.checked) {
+      temaSelecionado = radio.value;
+      break;
+    }
+  }
+
+  if (temaSelecionado) {
+    localStorage.setItem("temaPadrao", temaSelecionado);
+    alert("Tema padrão salvo com sucesso!");
+  } else {
+    alert("Por favor, selecione um tema antes de salvar.");
+  }
+}
+
+// Função para carregar o tema padrão salvo, se houver, ao carregar a página
+window.onload = function () {
+  const temaPadraoSalvo = localStorage.getItem("temaPadrao");
+
+  if (temaPadraoSalvo) {
+    const radios = document.getElementsByName("tema");
+
+    for (const radio of radios) {
+      if (radio.value === temaPadraoSalvo) {
+        radio.checked = true;
+        break;
+      }
+    }
+
+    themeDiv.dataset.tema = temaPadraoSalvo;
+  }
+};
