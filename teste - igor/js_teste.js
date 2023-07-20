ee2af950647820fa78809d7c9749a8abc5a0542f
Igor Oliveira lima <ol.igor98@gmail.com>
2023-07-19 23:42:53 -0300

    input radio theme test

--- teste - igor/js_teste.js
+++ teste - igor/js_teste.js
@@ -1,23 +1,3 @@
-const triggerMenuBurger = document.getElementById('menuBurger')
-const showBurguerItems = document.getElementById('burgerItems')
-
-triggerMenuBurger.addEventListener('click', function() {
-triggerMenuBurger.classList.toggle('is-active')
-showBurguerItems.classList.toggle('is-active')
-});
-
-const switchTheme = document.getElementById("switchTheme")
-const modal = document.querySelector("dialog")
-const buttonClose = document.querySelector("dialog")
-
-switchTheme.onclick = function() {
-  modal.showModal()
-}
-
-buttonClose.onclick = function() {
-  modal.close()
-}
-
 const switchFire = document.getElementsByClassName('fireTheme')[0];
 const switchGrass = document.getElementsByClassName('grassTheme')[0];
 const switchWater = document.getElementsByClassName('waterTheme')[0];
@@ -41,5 +21,3 @@ switchWater.addEventListener('click', function() {
   theme.classList.remove('water')
   theme.classList.add('water')
 });
-
- 
